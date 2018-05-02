954e835a1879139869a5bd859d07cb59210a76ea
Stephen Blum <blum.stephen@gmail.com>
2018-05-01 19:20:27 -0700

    starting to get audio implemented.

--- artificial.js
+++ artificial.js
@@ -2,8 +2,29 @@
     'use strict';
     const orbiter = document.getElementById('artificial');
     let   mood    = orbiter.className;
-    0&& setInterval( ()=>{
-        if (orbiter.className.indexOf('talking') > -1) orbiter.className = mood;
-        else                                           orbiter.className = mood + ' talking';
-    }, 1000 );
+    let   ting    = ['', 'listening', 'talking'];
+    let   pose    = 0;
+    setInterval( ()=>{
+        orbiter.className = mood + ting[pose++%3];
+    }, 2000 );
+})();
+
+(()=>{
+    'use strict';
+    let stream = null;
+    let source = null;
+
+    requestMicrophone();
+
+    function requestMicrophone() {
+        let constraints = { audio : { sampleRate:11000, channelCount:1 } };
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then( source => setSource(source) )
+        .catch( error => console.error(error) );
+    }
+
+    function setSource(src) {
+        stream = source;
+        //source = audioCtx.createMediaStreamSource(stream);
+    }
 })();

9cf34c7eeb16055888eec75bc26cb7ec5a0eedf7
Stephen Blum <blum.stephen@gmail.com>
2018-05-03 21:34:17 -0700

    fixed bug in story.

--- story.js
+++ story.js
@@ -11,9 +11,12 @@
         for ( let question in custom )
             if (ask.toLowerCase().indexOf(question.toLowerCase()) > -1)
                 return custom[question];
+
+        // Force Story Events
         return ({
-            //0 : "Wait a minute... okay I'm ready. I will be a happy robot."
-        ,   5 : "Sure. Anyway. Check PubNub.com I was built there."
+            0 : ""
+        , 100 : "Wait a minute... okay I'm ready. I will be a happy robot."
+        ,   5 : "Sure. Anyway. Check me out. I am a robot built inside a PubNub.com Function."
         })[''+count++];
     };
 
